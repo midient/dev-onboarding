80ad54cd08ce9496835c5c050ffcd4dbb7ac2a9a
husseinTalal2 <hossientalal2@gmail.com>
2022-05-26 13:10:05 +0300

    updating deleteTodo logic

--- src/services/useTodoStorage.ts
+++ src/services/useTodoStorage.ts
@@ -7,35 +7,30 @@ import { useEffect, useState } from "react";
 export function useTodoStorageService(persistence: PersistenceServices, idGen: idGeneratorService):TodoStorageService {
     const [todos, setTodos] = useState(Array<Todo>());
     
-    const getTodoIndex = (key: string): number => {
-        let todoIndex = -1;
-        todos.forEach((todo, index) => {
-            todoIndex = todo.id === key ? index : -1
-        })
-        return todoIndex;
+    const getTodoIndex = (id: string): number => {
+        let todo = todos.find(todo => todo.id === id);
+        return todo ? todos.indexOf(todo): -1;
     }
     
     useEffect(() =>{ 
         loadTodos()
-    }, [])
-
+    }, []);
 
     const addTodo = async (todo: {text: string}): Promise<void> => {
-        //loadTodos()
         const newTodo = {id: idGen(), text: todo.text} as Todo
         const newTodos = [...todos, newTodo]
         setTodos(newTodos);
-        //console.log(todos)
         await persistence.set("todos", JSON.stringify(newTodos))
     }
 
-    const deleteTodo = async (key: string) => {
-        const tempTodos = todos;
-        const todoIndex = getTodoIndex(key)
-        tempTodos.splice(todoIndex)
-        setTodos(tempTodos);
-        await persistence.set("todos", JSON.stringify(tempTodos))
-
+    const deleteTodo = async (id: string) => {
+        const tempTodos = [...todos];
+        const todoIndex = getTodoIndex(id);
+        if (todoIndex !== -1) {
+            tempTodos.splice(todoIndex, 1)
+            setTodos(tempTodos);
+            await persistence.set("todos", JSON.stringify(tempTodos));
+        }
     }
 
     const loadTodos = async () => {
